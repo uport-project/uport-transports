95536f3b4a4b6f3fa0fc7ab8d3dda73023097ef3
Zach Ferland <zachferland@gmail.com>
2018-05-01 15:14:14 -0400

    feat: move subprovider from uport-connect to here

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as transport from './transport/index.js'
 import * as message from './message/index.js'
 import * as crypto from './crypto/index.js'
+import * as provider from './provider.js'
 
-export { transport, message, crypto }
+export { transport, message, crypto, provider }
