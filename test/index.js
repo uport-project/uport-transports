3fbae6d8f559b0ad9ebe46372ce8ed7397d12186
Zach Ferland <zachferland@gmail.com>
2018-08-13 19:06:22 -0400

    test: ignore css and svg files for tests

--- test/index.js
+++ test/index.js
@@ -1,3 +1,7 @@
+require('babel-register')
+require.extensions['.css'] = () => {};
+require.extensions['.svg'] = () => {};
+
 require('./transport/index.js')
 require('./message/index.js')
 require('./crypto-test.js')
