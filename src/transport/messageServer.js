2bd0f8e2e093264b23bca71011efd4ee1c4fbf29
Zach Ferland <zachferland@gmail.com>
2018-08-21 19:00:05 -0400

    feat: add attest req support to getCallback

--- src/transport/messageServer.js
+++ src/transport/messageServer.js
@@ -73,7 +73,12 @@ const formatMessageServerUrl = (url) => {
 }
 const genCallback = (messageServerUrl = CHASQUI_URL) => `${formatMessageServerUrl(messageServerUrl)}${randomString(16)}`
 const isMessageServerCallback = (uri, messageServerUrl = CHASQUI_URL) => new RegExp(formatMessageServerUrl(messageServerUrl)).test(getCallback(uri))
-const getCallback = (uri) => decodeJWT(getURLJWT(uri)).payload.callback
+const getCallback = (uri) => {
+  const tokenCB = decodeJWT(getURLJWT(uri)).payload.callback
+  if (tokenCB) return tokenCB
+  // support attest req, which does not included cb in token, attest req may better align with other requests in the future
+  return getUrlQueryParams(uri).callback_url
+}
 
 
 export { URIHandlerSend,
