b10e5eec05e52d09cdabfa09dc6310994dc1c62b
Rob Whitaker <rmw2@princeton.edu>
2018-10-19 13:53:57 -0400

    feat: separate url params in chasqui topic
    Move messageToUri parsing inside chasquiCompress function
    Add test for message format in qr.chasquiCompress
    
    All url parameters in a sufficiently large message given to chasquiCompress will now be encoded as key-value pairs in the JSON payload that gets posted to chasqui.  This simplifies parsing on the mobile side after receiving a chasqui url as a message.

--- test/transport/qr.js
+++ test/transport/qr.js
@@ -1,10 +1,9 @@
 const proxyquire = require('proxyquire')
 const chai = require('chai')
 const sinon = require('sinon')
+const { messageToURI } = require('../../src/message/util')
 const expect = chai.expect
 chai.use(require('sinon-chai'))
-
-const { isMessageServerCallback } = require('../../src/transport/messageServer')
 
 // Exactly 650 characters
 const fakeJWT = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMiwiY2xhaW0iOiJsYWxhbGFsYWxhbGFsYWxhbGFsYWxhbGFsYWxhbGFsYWxhbGFsYWxhbGFsYWxsYWxhbGFsYWxhbGFsYWxhbGFsYWxhbGFzZmphbGtmandvZWZqcGFvaXdqZnBramF3ZWZwb2lqZmVyaW9mam9pYXdqZmVvamFvc2lmam9haWVqZm9pYXdlam9maWphb2lzZWpmb2lhc2plZm9pYWpwb3dldWhmcWxsa2MsZG0sZG1kbWRtYXNtYSx3Zml3ZWYzMHN3ZWY0dDk4dTg0aGcwM3VoNGcwaXUzaGdmMG9pcnVuMDR1cm9mbHF1NDU5aG9xODV1NTA5ODN1NDA5ODIzdTR0b2lqZmxpamloYXNmZWFmZmVmZWVlZWVhc2V5dWllamZpZWplbGZpM2pmOTgzamY5MnVqa3NyZGZsaWoifQ.pcofwu5zy5i7SAQpCCa5kIAFMtsSSrSyLD8QG_Mkrh-Xjfo8U9xdB5u2575wCiFUHUG2GXWKM4DBM6yIZQVlLA'
@@ -24,7 +23,7 @@ describe('qr send', () => {
 })
 
 describe('chasquiCompress', () => {
-  it('does nothing for messages below the threshold', () => {
+  it('formats messages below the threshold as uris', () => {
     const message = 'shortmessage'
     const nets = sinon.spy()
     const { chasquiCompress } = proxyquire('../../src/transport/qr', {
@@ -32,22 +31,31 @@ describe('chasquiCompress', () => {
     })
 
     return chasquiCompress(message).then((msg) => {
-      expect(msg).to.equal(message)
+      expect(msg).to.equal(messageToURI(message) + '?callback_type=post')
       expect(nets).to.not.be.called
     })
   })
 
   it('uploads to chasqui for long messages, and returns an encoded topicUrl', () => {
-    const threshold = 650
-    const nets = sinon.stub().callsFake((_, cb) => cb(null, {
-      statusCode: 201,
-      headers: {Location: '/topic/deadbeefDEADBEEF'}
-    }))
+    const callback_type = 'post'
+    const callback_url = 'uport.me'
+    const message = `${fakeJWT}?callback_url=${callback_url}&callback_type=${callback_type}`;
+
+    const nets = sinon.stub().callsFake(({body}, cb) => {
+      body = JSON.parse(body)
+      expect(body.message).to.equal(fakeJWT)
+      expect(body.callback_type).to.equal(callback_type)
+      expect(body.callback_url).to.equal(callback_url)
+      cb(null, {
+        statusCode: 201,
+        headers: {Location: '/topic/deadbeefDEADBEEF'}
+      })
+    })
     const { chasquiCompress } = proxyquire('../../src/transport/qr', {
       'nets': nets
     })
 
-    return chasquiCompress(fakeJWT).then((msg) => {
+    return chasquiCompress(message).then((msg) => {
       expect(decodeURI(msg)).to.match(/\/topic\//)
       expect(nets).to.be.calledOnce
     })
