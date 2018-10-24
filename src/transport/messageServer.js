a9e1d3ceb938cf68731fd756f292c3a11c8ebf00
Mike Xu <mxu11290@gmail.com>
2018-10-24 19:19:13 -0400

    set up eslint-config-uport and fix errors

--- src/transport/messageServer.js
+++ src/transport/messageServer.js
@@ -10,28 +10,31 @@ const POLLING_INTERVAL = 2000
 // TODO should it just allow cancel func to be passed in??
 // TODO Should it return uri append to promise? instead of a promise??
 /**
-  *  A general Chasqui Transport. Allows you to configure the transport with any uriHandler for the request,
-  *  while the response will always be returned through Chasqui. Chasqui is a simple messaging server that
-  *  allows responses to be relayed from a uport client to the original callee.
-  *
-  *  @param    {String}       uriHandler               a function called with the requestURI once it is formatted for this transport
-  *  @param    {Object}       [config={}]              an optional config object
-  *  @param    {String}       [config.chasquiUrl]      url of messaging server, defaults to Chasqui instance run by uPort
-  *  @param    {String}       [config.pollingInterval] milisecond interval at which the messaging server will be polled for a response
-  *  @return   {Function}                              a configured QRTransport Function
-  *  @param    {String}       message                  a uPort client request message
-  *  @return   {Promise<Object, Error>}                a function to close the QR modal
-  */
-const URIHandlerSend = (uriHandler, {messageServerUrl = CHASQUI_URL, pollingInterval = POLLING_INTERVAL} = {}) => {
+ *  A general Chasqui Transport. Allows you to configure the transport with any uriHandler for the request,
+ *  while the response will always be returned through Chasqui. Chasqui is a simple messaging server that
+ *  allows responses to be relayed from a uport client to the original callee.
+ *
+ *  @param    {String}       uriHandler               a function called with the requestURI once it is formatted for this transport
+ *  @param    {Object}       [config={}]              an optional config object
+ *  @param    {String}       [config.chasquiUrl]      url of messaging server, defaults to Chasqui instance run by uPort
+ *  @param    {String}       [config.pollingInterval] milisecond interval at which the messaging server will be polled for a response
+ *  @return   {Function}                              a configured QRTransport Function
+ *  @param    {String}       message                  a uPort client request message
+ *  @return   {Promise<Object, Error>}                a function to close the QR modal
+ */
+const URIHandlerSend = (uriHandler, { messageServerUrl = CHASQUI_URL, pollingInterval = POLLING_INTERVAL } = {}) => {
   if (!uriHandler) throw new Error('uriHandler function required')
   return (message, params = {}) => {
     let uri = messageToURI(message)
     const callback = getCallback(uri)
-    if (!isMessageServerCallback(uri, messageServerUrl)) throw new Error('Not a request that can be handled by this configured messaging server transport')
+    if (!isMessageServerCallback(uri, messageServerUrl))
+      throw new Error('Not a request that can be handled by this configured messaging server transport')
     let isCancelled = false
-    const cancel = () => { isCancelled = true }
-    uri = paramsToQueryString(uri, {'callback_type': 'post'})
-    uriHandler(uri, Object.assign(params, {cancel}))
+    const cancel = () => {
+      isCancelled = true
+    }
+    uri = paramsToQueryString(uri, { callback_type: 'post' })
+    uriHandler(uri, Object.assign(params, { cancel }))
     const returnVal = poll(callback, pollingInterval, () => isCancelled)
     returnVal.cancel = cancel
     return returnVal
@@ -39,28 +42,25 @@ const URIHandlerSend = (uriHandler, {messageServerUrl = CHASQUI_URL, pollingInte
 }
 
 /**
-  *  A polling function specifically for polling Chasqui.
-  *
-  *  @param    {String}                  url                a Chasqui url polled
-  *  @param    {Integer}                 [pollingInterval]  ms interval at which the given url is polled
-  *  @param    {Function}                [cancelled]        function which returns boolean, if returns true, polling stops
-  *  @return   {Promise<Object, Error>}                     a promise which resolves with obj/message or rejects with an error
-  */
-const poll = (url, pollingInterval, cancelled ) => {
-  const messageParse = (res) => { 
-    const message = res.message && (
-      res.message.content 
-        // New chasqui format
-        ? JSON.parse(res.message.content) 
-        // Old chasqui format
-        : res.message
-    )
+ *  A polling function specifically for polling Chasqui.
+ *
+ *  @param    {String}                  url                a Chasqui url polled
+ *  @param    {Integer}                 [pollingInterval]  ms interval at which the given url is polled
+ *  @param    {Function}                [cancelled]        function which returns boolean, if returns true, polling stops
+ *  @return   {Promise<Object, Error>}                     a promise which resolves with obj/message or rejects with an error
+ */
+const poll = (url, pollingInterval, cancelled) => {
+  const messageParse = res => {
+    // Support new and old chasqui format
+    const message = res.message && (res.message.content ? JSON.parse(res.message.content) : res.message)
     // FIXME: this is not a great method for handling our expected keys in the response.
     //        Very tightly coupled to the message format, and these keys seem to come out
     //        of nowhere.
     return message['access_token'] || message['tx'] || message['typedDataSig']
   }
-  const errorParse = (res) => { if (res.message) return res.message.error }
+  const errorParse = res => {
+    if (res.message) return res.message.error
+  }
   return generalPoll(url, messageParse, errorParse, cancelled, pollingInterval).then(res => {
     clearResponse(url)
     return res
@@ -68,35 +68,36 @@ const poll = (url, pollingInterval, cancelled ) => {
 }
 
 // TODO maybe remove and just have reasonable removal times
-const clearResponse = (url) => {
-  nets({
-    uri: url,
-    method: 'DELETE',
-    withCredentials: false,
-    rejectUnauthorized: false
-  }, function (err) { if (err) { throw err } /* Errors without this cb */ })
+const clearResponse = url => {
+  nets(
+    {
+      uri: url,
+      method: 'DELETE',
+      withCredentials: false,
+      rejectUnauthorized: false,
+    },
+    function(err) {
+      if (err) {
+        throw err
+      } /* Errors without this cb */
+    },
+  )
 }
 
-const formatMessageServerUrl = (url) => {
+const formatMessageServerUrl = url => {
   if (url.endsWith('/topic/')) return url
-  if (url.endsWith('/topic'))  return `${url}/`
-  if (url.endsWith('/'))       return `${url}topic/`
+  if (url.endsWith('/topic')) return `${url}/`
+  if (url.endsWith('/')) return `${url}topic/`
   return `${url}/topic/`
 }
 const genCallback = (messageServerUrl = CHASQUI_URL) => `${formatMessageServerUrl(messageServerUrl)}${randomString(16)}`
-const isMessageServerCallback = (uri, messageServerUrl = CHASQUI_URL) => new RegExp(formatMessageServerUrl(messageServerUrl)).test(getCallback(uri))
-const getCallback = (uri) => {
+const isMessageServerCallback = (uri, messageServerUrl = CHASQUI_URL) =>
+  new RegExp(formatMessageServerUrl(messageServerUrl)).test(getCallback(uri))
+const getCallback = uri => {
   const tokenCB = decodeJWT(getURLJWT(uri)).payload.callback
   if (tokenCB) return tokenCB
   // support attest req, which does not included cb in token, attest req may better align with other requests in the future
   return getUrlQueryParams(uri).callback_url
 }
 
-
-export { URIHandlerSend,
-         poll,
-         clearResponse,
-         genCallback,
-         isMessageServerCallback,
-         CHASQUI_URL
-       }
+export { URIHandlerSend, poll, clearResponse, genCallback, isMessageServerCallback, CHASQUI_URL }
--- src/transport/qr.js
+++ src/transport/qr.js
@@ -1,8 +1,8 @@
 import nets from 'nets'
 
-import { open, close, success, failure } from './ui'
+import { open, close, failure } from './ui'
 import { paramsToQueryString, messageToURI, getUrlQueryParams } from './../message/util.js'
-import { URIHandlerSend, CHASQUI_URL, genCallback } from './messageServer'
+import { URIHandlerSend, CHASQUI_URL } from './messageServer'
 
 const POLLING_INTERVAL = 2000
 
@@ -15,23 +15,25 @@ const POLLING_INTERVAL = 2000
  *   @param    {String}       message            a uport client request message
  *   @param    {Object}       [opts={}]
  *   @param    {Function}     [opts.cancel]      cancel callback, called on modal close
- *   @param    {Function}     [opts.compress]    a function to compress a JWT, returning a promise that resolves to a string 
+ *   @param    {Function}     [opts.compress]    a function to compress a JWT, returning a promise that resolves to a string
  *   @return   {Function}                        a function to close the QR modal
  */
-const send = (displayText) => (message, {cancel, compress} = {}) => {
+const send = displayText => (message, { cancel, compress } = {}) => {
   if (compress) {
-    compress(message).then((msg) => {
-      open(msg, cancel, displayText)
-    }).catch((err) => {
-      console.error(err)
-      // Display failure modal and allow retry
-      failure(() => send(displayText)(message, {cancel, compress}))
-    })
+    compress(message)
+      .then(msg => {
+        open(msg, cancel, displayText)
+      })
+      .catch(err => {
+        console.error(err)
+        // Display failure modal and allow retry
+        failure(() => send(displayText)(message, { cancel, compress }))
+      })
   } else {
     let uri = messageToURI(message)
-    uri = /callback_type=/.test(uri) ? uri : paramsToQueryString(uri, {callback_type: 'post'})
-    open(uri, cancel, displayText)   
-  }  
+    uri = /callback_type=/.test(uri) ? uri : paramsToQueryString(uri, { callback_type: 'post' })
+    open(uri, cancel, displayText)
+  }
 
   // Return close function immediately, UI is async anyway
   return close
@@ -40,65 +42,72 @@ const send = (displayText) => (message, {cancel, compress} = {}) => {
 /**
  * A utility function for reducing the size of a QR code by uploading it to chasqui and
  * replacing the contents with the topic url.
- * 
+ *
  * An empty Verification JWT (i.e. with signature and sub/iss but blank claim) is ~250 characters
  * The absolute max that can fit in a scanable QR on the screen is ~1500 characters
  * Below 650 characters the QR modal fits perfectly in the browser on a 13" MBP, with some wiggle room
  *
  * @param   {String}  message     the request JWT
  * @param   {Number}  threshold   the smallest size (in string length) to compress
- * @returns {String}  the chasqui url of the message, or the original message if less than threshold 
+ * @returns {String}  the chasqui url of the message, or the original message if less than threshold
  */
-const chasquiCompress = (message, threshold=650) => {
+const chasquiCompress = (message, threshold = 650) => {
   return new Promise((resolve, reject) => {
     if (message.length < threshold) {
       let uri = messageToURI(message)
-      uri = /callback_type=/.test(uri) ? uri : paramsToQueryString(uri, {callback_type: 'post'})
+      uri = /callback_type=/.test(uri) ? uri : paramsToQueryString(uri, { callback_type: 'post' })
       resolve(uri)
-      return 
+      return
     }
     // Trim message and extract query params
     const topic = {
       message: message.replace(/\?.*/, ''),
-      ...getUrlQueryParams(message) 
+      ...getUrlQueryParams(message),
     }
-    nets({
-      uri: `${CHASQUI_URL}topic/`,
-      method: 'POST',
-      body: JSON.stringify(topic),
-      headers: {
-        'content-type': 'application/json'
+    nets(
+      {
+        uri: `${CHASQUI_URL}topic/`,
+        method: 'POST',
+        body: JSON.stringify(topic),
+        headers: {
+          'content-type': 'application/json',
+        },
+        withCredentials: false,
+        rejectUnauthorized: false,
       },
-      withCredentials: false,
-      rejectUnauthorized: false
-    }, function (err, response) {
-      if (err) reject(err)
-      else if (response.statusCode !== 201) reject('Failed to create topic')
-      else resolve(response.headers.location || response.headers.Location)      
-    })
+      function(err, response) {
+        if (err) reject(err)
+        else if (response.statusCode !== 201) reject(new Error('Failed to create topic'))
+        else resolve(response.headers.location || response.headers.Location)
+      },
+    )
   })
 }
 
 /**
-  *  A QR Code and Chasqui Transport. The QR modal is configured for tranporting the request, while the
-  *  response will be returned through Chasqui.
-  *
-  *  @param    {Object}       [config={}]               an optional config object
-  *  @param    {String}       [config.chasquiUrl]       url of messaging server, defaults to Chasqui instance run by uPort
-  *  @param    {String}       [config.pollingInterval]  milisecond interval at which the messaging server will be polled for a response
-  *  @return   {Function}                               a configured QRTransport Function
-  *  @param    {String}       message                   a uPort client request message
-  *  @return   {Promise<Object, Error>}                 a function to close the QR modal
-  */
+ *  A QR Code and Chasqui Transport. The QR modal is configured for tranporting the request, while the
+ *  response will be returned through Chasqui.
+ *
+ *  @param    {Object}       [config={}]               an optional config object
+ *  @param    {String}       [config.chasquiUrl]       url of messaging server, defaults to Chasqui instance run by uPort
+ *  @param    {String}       [config.pollingInterval]  milisecond interval at which the messaging server will be polled for a response
+ *  @return   {Function}                               a configured QRTransport Function
+ *  @param    {String}       message                   a uPort client request message
+ *  @return   {Promise<Object, Error>}                 a function to close the QR modal
+ */
 const chasquiSend = ({ chasquiUrl = CHASQUI_URL, pollingInterval = POLLING_INTERVAL, displayText } = {}) => {
-  const transport = URIHandlerSend(send(displayText, {compress: chasquiCompress}), {chasquiUrl, pollingInterval})
-  return (message, params) => transport(message, params).then(res => {
-    close()
-    return res
-  }, err => {
-    close()
-    throw new Error(err)
-  })
+  const transport = URIHandlerSend(send(displayText, { compress: chasquiCompress }), { chasquiUrl, pollingInterval })
+  return (message, params) =>
+    transport(message, params).then(
+      res => {
+        close()
+        return res
+      },
+      err => {
+        close()
+        throw new Error(err)
+      },
+    )
 }
 
 export { chasquiSend, chasquiCompress, send }
