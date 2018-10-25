08c4936d17bc6d9c7a8be718d4edda6699e23617
Rob Whitaker <rmw2@princeton.edu>
2018-10-25 08:37:48 -0400

    revert: replace chasqui url with old

--- test/transport/chasqui.js
+++ test/transport/chasqui.js
@@ -6,7 +6,7 @@ var chai = require('chai');
 const expect = chai.expect
 chai.use(require('sinon-chai'))
 let chasqui = require('./../../src/transport/messageServer.js')
-const CHASQUI_URL = 'https://api.uport.space/chasqui/topic/'
+const CHASQUI_URL = 'https://chasqui.uport.me/api/v1/topic/'
 const ranStr = 'aVKcEd5jznmWslj3'
 
 // TODO stub nets everywhere
@@ -25,7 +25,9 @@ describe('transport.chasqui', function () {
     const uriHandler = sinon.spy()
     const pollGeneral = sinon.stub().resolves('ResponseString')
     const randomString = sinon.stub().returns(ranStr)
-    const requestMessage = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1Mjk5NTQxMjcsImV4cCI6MTUyOTk1NDcyNywicmVxdWVzdGVkIjpbIm5hbWUiLCJwaG9uZSIsImNvdW50cnkiXSwicGVybWlzc2lvbnMiOlsibm90aWZpY2F0aW9ucyJdLCJjYWxsYmFjayI6Imh0dHBzOi8vYXBpLnVwb3J0LnNwYWNlL2NoYXNxdWkvdG9waWMvYVZLY0VkNWp6bm1Xc2xqMyIsInR5cGUiOiJzaGFyZVJlcSIsImlzcyI6ImRpZDpldGhyOjB4NDFjOTkxZjczZjBjN2E3Y2M5NWI5ZGY5MDk0NmUxYTdiMDE3NGVlNiJ9.hjkP5-yoAACebORa29-iO_VXCm3_81KwaHzrD3xHI94PbJzaT50x3uSr9MwQSl9sO1Lmdzkg1VMkuHyHFt9ClAE'
+    const requestMessage = CHASQUI_URL.match(/api\.uport/) 
+      ? 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1Mjk5NTQxMjcsImV4cCI6MTUyOTk1NDcyNywicmVxdWVzdGVkIjpbIm5hbWUiLCJwaG9uZSIsImNvdW50cnkiXSwicGVybWlzc2lvbnMiOlsibm90aWZpY2F0aW9ucyJdLCJjYWxsYmFjayI6Imh0dHBzOi8vYXBpLnVwb3J0LnNwYWNlL2NoYXNxdWkvdG9waWMvYVZLY0VkNWp6bm1Xc2xqMyIsInR5cGUiOiJzaGFyZVJlcSIsImlzcyI6ImRpZDpldGhyOjB4NDFjOTkxZjczZjBjN2E3Y2M5NWI5ZGY5MDk0NmUxYTdiMDE3NGVlNiJ9.hjkP5-yoAACebORa29-iO_VXCm3_81KwaHzrD3xHI94PbJzaT50x3uSr9MwQSl9sO1Lmdzkg1VMkuHyHFt9ClAE'
+      : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1Mjk5NTQxMjcsImV4cCI6MTUyOTk1NDcyNywicmVxdWVzdGVkIjpbIm5hbWUiLCJwaG9uZSIsImNvdW50cnkiXSwicGVybWlzc2lvbnMiOlsibm90aWZpY2F0aW9ucyJdLCJjYWxsYmFjayI6Imh0dHBzOi8vY2hhc3F1aS51cG9ydC5tZS9hcGkvdjEvdG9waWMvYVZLY0VkNWp6bm1Xc2xqMyIsInR5cGUiOiJzaGFyZVJlcSIsImlzcyI6ImRpZDpldGhyOjB4OWY0Yjc2ZDI2ODJhZmY1NmJjODdhNTkwNzJlNTAyNWExMjA3YTdmOSJ9.TdkEhbhOXEMQtNdqk3c9U8lueY2gy4tD0QhqGm_HMawFI-wPXz4nf2FFfdb_UYCBR03dZnrBqcyQz8rXBab8tQE'
     const uri = `https://id.uport.me/req/${requestMessage}`
     let URIHandlerSend
 
