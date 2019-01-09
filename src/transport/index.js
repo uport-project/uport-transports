9a2d5b30954c25b34b48b410b7418ceb72dd4185
Rob Whitaker <rmw2@princeton.edu>
2019-01-09 16:24:57 -0700

    feat: update transport/index to export proper modules

--- src/transport/index.js
+++ src/transport/index.js
@@ -1,8 +1,6 @@
 import * as qr from './qr'
-import * as ui from './ui/index'
-import * as messageServer from './messageServer'
-import * as push from './push'
 import * as url from './url'
-import poll from './poll'
+import * as pubsub from './pubsub'
+import * as push from './push'
 
-export { qr, push, messageServer, poll, url, ui }
+export { pubsub, push, url, qr }
