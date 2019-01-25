5b32f55f937f4f660b6c1e8bd4b4dbd8a4dfb108
Rob Whitaker <rmw2@princeton.edu>
2019-01-25 09:51:14 -0500

    fix: remove duplicate definition of compose

--- src/index.js
+++ src/index.js
@@ -3,8 +3,3 @@ import * as helpers from './helpers'
 import * as network from './network'
 
 export { helpers, transport, network }
-
-const compose = (...fns) => msg => fns.reduce((x, fn) => fn(x), msg)
-
-compose(helpers.messageToUri, push.createSender())
-compose(push.createListener(), helpers.uriToMessage)
