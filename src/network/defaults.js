1fcd44f668dbfc76c4d3d3cec396c1dd7f111cc2
Rob Whitaker <rmw2@princeton.edu>
2019-02-12 11:20:51 -0500

    feat: update infura urls

--- src/network/defaults.js
+++ src/network/defaults.js
@@ -4,22 +4,22 @@ const networks = {
   mainnet: {
     id: '0x1',
     registry: '0xab5c8051b9a1df1aab0149f8b0630848b7ecabf6',
-    rpcUrl: 'https://mainnet.infura.io',
+    rpcUrl: 'https://mainnet.infura.io/v3/e72b472993ff46d3b5b88faa47214d7f',
   },
   ropsten: {
     id: '0x3',
     registry: '0x41566e3a081f5032bdcad470adb797635ddfe1f0',
-    rpcUrl: 'https://ropsten.infura.io',
+    rpcUrl: 'https://ropsten.infura.io/v3/e72b472993ff46d3b5b88faa47214d7f',
   },
   kovan: {
     id: '0x2a',
     registry: '0x5f8e9351dc2d238fb878b6ae43aa740d62fc9758',
-    rpcUrl: 'https://kovan.infura.io',
+    rpcUrl: 'https://kovan.infura.io/v3/e72b472993ff46d3b5b88faa47214d7f',
   },
   rinkeby: {
     id: '0x4',
     registry: '0x2cc31912b2b0f3075a87b3640923d45a26cef3ee',
-    rpcUrl: 'https://rinkeby.infura.io',
+    rpcUrl: 'https://rinkeby.infura.io/v3/e72b472993ff46d3b5b88faa47214d7f',
   },
 }
 
