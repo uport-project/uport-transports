a5c8e2592e12cfbac0185d4f32d3fe11f2885c25
Rob Whitaker <rmw2@princeton.edu>
2019-02-25 13:19:40 -0500

    feat: add definition of compose function

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import * as transport from './transport'
 import * as helpers from './helpers'
 import * as network from './network'
 
-export { helpers, transport, network }
+/**
+ * Compose a list of arbitrary functions, returning a function which applies them
+ * each to a single argument in reverse order.
+ * @param   {...Function} fns A comma-separated list of functions
+ * @returns {Function}
+ */
+const compose = (...fns) => arg => fns.reduceRight((val, fn) => fn(val), arg)
+
+export { compose, helpers, transport, network }
